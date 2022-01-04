bdec521fad31cec9dd7382679d2449a25119ecbe
Josh <joshua.mcswain@mecklenburgcountync.gov>
2022-01-04 11:46:42 -0500

    I added items with images to the seeds.js

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -5,11 +5,11 @@ db.once('open', async () => {
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
-    { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
+    { name: 'Livingroom' },
+    { name: 'Kitchen' },
+    { name: 'Bedroom' },
+    { name: 'Outdoor' },
+    { name: 'Bathroom' }
   ]);
 
   console.log('categories seeded');
@@ -18,111 +18,111 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Tin of Cookies',
+      name: 'Couch',
       description:
-        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
+        'Description of couch.',
+      image: 'couch.jpg',
       category: categories[0]._id,
-      price: 2.99,
-      quantity: 500
+      price: 45.99,
+      quantity: 1
     },
     {
-      name: 'Canned Coffee',
+      name: 'Dinning table',
       description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
+        'Description of table.',
+      image: 'dinning-table.jpg',
       category: categories[0]._id,
-      price: 1.99,
-      quantity: 500
+      price: 50.99,
+      quantity: 1
     },
     {
-      name: 'Toilet Paper',
+      name: 'Bed',
       category: categories[1]._id,
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
-      price: 7.99,
-      quantity: 20
+        'Description of bed.',
+      image: 'bed.jpg',
+      price: 85.99,
+      quantity: 1
     },
     {
-      name: 'Handmade Soap',
+      name: 'Glider',
       category: categories[1]._id,
       description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
-      price: 3.99,
-      quantity: 50
+        'Description of outdoor glider',
+      image: 'glidder.jpg',
+      price: 13.99,
+      quantity: 1
     },
     {
-      name: 'Set of Wooden Spoons',
+      name: 'Makeup desk',
       category: categories[1]._id,
       description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
+        'Description of makeup desk.',
+      image: 'makeup-desk.jpg',
       price: 14.99,
-      quantity: 100
+      quantity: 1
     },
     {
-      name: 'Camera',
+      name: 'Recliner',
       category: categories[2]._id,
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
-      price: 399.99,
-      quantity: 30
+        'Desription of recliner.',
+      image: 'recliner.jpg',
+      price: 39.99,
+      quantity: 1
     },
     {
-      name: 'Tablet',
+      name: 'Bar Stool',
       category: categories[2]._id,
       description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
+        'Description of bar stool.',
+      image: 'bar-stool.jpg',
+      price: 4.99,
+      quantity: 1
     },
     {
-      name: 'Tales at Bedtime',
+      name: 'Knightstand',
       category: categories[3]._id,
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
-      price: 9.99,
-      quantity: 100
+        'Description of nightstand.',
+      image: 'nightstand.jpg',
+      price: 19.99,
+      quantity: 1
     },
     {
-      name: 'Spinning Top',
+      name: 'outdoor table',
       category: categories[4]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
-      price: 1.99,
-      quantity: 1000
+      description: 'Description of outdoor table',
+      image: 'outdoor-table.jpg',
+      price: 35.99,
+      quantity: 1
     },
     {
-      name: 'Set of Plastic Horses',
-      category: categories[4]._id,
-      description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
-      price: 2.99,
-      quantity: 1000
-    },
-    {
-      name: 'Teddy Bear',
+      name: 'Linen Shelf',
       category: categories[4]._id,
       description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
-      price: 7.99,
-      quantity: 100
+        'Description of linen shelf.',
+      image: 'linen-shelf.jpg',
+      price: 12.99,
+      quantity: 1
     },
     {
-      name: 'Alphabet Blocks',
+      name: 'dresser',
       category: categories[4]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
-      price: 9.99,
-      quantity: 600
+        'Description of dresser.',
+      image: 'dresser.jpg',
+      price: 27.99,
+      quantity: 1
+    },
+    {
+      name: 'ottoman',
+      category: categories[4]._id,
+      description:
+        'Desription of ottoman.',
+      image: 'ottoman.jpg',
+      price: 29.99,
+      quantity: 1
     }
   ]);
 
