0e1847d3f0ebedca79f6d31281d696af42213856
Josh <joshua.mcswain@mecklenburgcountync.gov>
2022-01-03 20:50:34 -0500

    push schema

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,52 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveItem(input: savedItem!): User
+  removeItem(itemId: ID!): User
+}
+input savedItem {
+  itemId: String
+  sellers: [String]
+  description: String
+  title: String
+  image: String
+  
+}
+
+type User {
+  _id: ID!
+  username: String
+  email: String
+  itemCount: Int
+  savedItems: [Item]
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+  type Item {
+    itemId: String
+    sellerss: [String]
+    # sellers: String
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+ 
+
+`;
+
+module.exports = typeDefs;
