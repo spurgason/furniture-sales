65fb9183c135f7bf8457a432767b80e092c7aaef
trswork <87744214+trswork@users.noreply.github.com>
2022-01-06 23:11:14 -0500

    add home

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Cart from "../components/Cart";
+
+const Home = () => {
+    return (
+      <div className="container">
+        <Cart />
+      </div>
+    );
+  };
+
+export default Home;
