4b11bbc8264422dd9bc1b34c5ef18b24fdc5a6e9
trswork <87744214+trswork@users.noreply.github.com>
2022-01-08 20:00:23 -0500

    update cart

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ItemList from "../components/ItemList";
 import CategoryMenu from "../components/CategoryMenu";
+import Cart from "../components/Cart";
 
 const Home = () => {
     return (
         <div className="container">
             <CategoryMenu />
             <ItemList />
+            <Cart />
         </div>
     )
 }
