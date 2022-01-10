08362b70dfbec05edfa613f8653c1f4f34e51986
Josh <joshua.mcswain@mecklenburgcountync.gov>
2022-01-10 16:51:50 -0500

    fixed issues with signup and login, tokens, and frontend backend connection

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -34,16 +34,7 @@ export const ADD_USER = gql`
         _id
         username
         email
-        bookCount
-        savedBooks {
-          authors
-          bookId
-          image
-          link
-          title
-          description
         }
-      }
     }
   }
 `;
--- server/server.js
+++ server/server.js
@@ -11,26 +11,29 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // context: authMiddleware,
-  introspection: true
+  context: authMiddleware,
+  // introspection: true
 });
 
-server.applyMiddleware({ app });
+server.start().then(() => {
+  server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/public")));
-}
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
-});
-
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/public")));
+  }
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
   });
-});
+  
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+})
+
